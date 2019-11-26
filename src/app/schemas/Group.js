cd516ecdca0ff1dd0195ccb66335b1067bf706ed
Wellington Calixto <wac.monti@outlook.com>
2019-11-26 15:03:14 -0200

    GroupSchema changes

--- src/app/schemas/Group.js
+++ src/app/schemas/Group.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 
 const GroupSchema = new mongoose.Schema({
-  themes: { type: String, required: true },
-  description: { type: String, required: true },
-  validate: { type: Date, required: true },
-  level_dificult: { type: Number, require: true },
-  id_user: { type: Number, required: true },
+  name: { type: String, required: true },
+  interests: { type: String, required: true },
+  id_admin: { type: Number, required: true },
+  members: { type: Object },
+  members_validate: { type: Object, default: null },
+  group_validate: { type: Boolean, default: false },
+  group_pontuation: { type: Number, default: 0 },
 });
 
 export default mongoose.model('Group', GroupSchema);
