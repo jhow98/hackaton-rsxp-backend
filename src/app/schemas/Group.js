75afe6e6b9a83a8e47537d83f4171aed490d9951
Wellington Calixto <wac.monti@outlook.com>
2019-11-26 16:11:59 -0200

    rename eventschema e challengeschama

--- src/app/schemas/Group.js
+++ src/app/schemas/Group.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 const GroupSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  interests: { type: Object, required: true},
+  interests: { type: Object, required: true },
   admin_id: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "User"
-},
-  members: { type: Object, required: true}
+    ref: 'User',
+  },
+  members: { type: Object, required: true },
 });
 
 export default mongoose.model('Group', GroupSchema);
