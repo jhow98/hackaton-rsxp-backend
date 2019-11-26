3cf7217416ce0af80b448e742832bdcb643e838f
jhow98 <jhonatan@fho.edu.br>
2019-11-26 13:49:26 -0300

    add initial routes

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,28 @@
-import {Router} from 'express'
+import express from 'express';
 
-const routes = new Router()
+import UserController from '../src/app/controllers/UserController'
+import DashboardController from '../src/app/controllers/DashboardController'
+import EventController from '../src/app/controllers/EventController'
 
-routes.get('/', (req, res) => {
-  return res.json('teste')
-})
+const routes = express.Router();
 
+routes.get('/events', EventController.get); 
+routes.post('/events/', EventController.post); 
+routes.delete('/events', EventController.delete); 
+routes.put('/events', EventController.put); 
 
-export default routes
+routes.get('/group', GroupController.get); 
+routes.post('/group/', GroupController.post); 
+routes.delete('/group', GroupController.delete); 
+routes.put('/group', GroupController.put); 
+
+routes.get('/users', UserController.get); 
+routes.post('/users/', UserController.post); 
+routes.delete('/users', UserController.delete); 
+routes.put('/users', UserController.put);  
+
+routes.get('/dashboard', DashboardController.show); 
+
+routes.post('/spots/:spot_id/bookings', BookingController.store)
+
+module.exports = routes;
